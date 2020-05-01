59d82a6bb9956dc836c8064f33fd864958dfc8a8
Sumit Chandak <38399572+sumitc29@users.noreply.github.com>
2020-05-01 15:52:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def index():
     # rendering webpage
     return render_template('index.html')
 
-def gen(camera):
+'''def gen(camera):
     while True:
         #get camera frame
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')'''
 
 
 @app.route('/video_feed')
